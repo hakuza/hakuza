d4bc0dd12c6c924ddfefdbb0510a310f48bb065e
Yuehan Duan <yuehanduan@yahoo.com>
2018-02-23 14:31:24 -0800

    Create db.

--- feedback/server/router.js
+++ feedback/server/router.js
@@ -1,26 +1,11 @@
-var express = require("express");
-var router = express.Router();
-var rp = require("request-promise");
+const express = require("express");
+const router = express.Router();
+const Review = require("./../database/index.js");
 
 router.get("/", function(req, res) {
-  async function getReviews() {
-    var options = {
-      method: "GET",
-      uri: "https://www.udemy.com/api-2.0/courses",
-      headers: {
-        Authorization:
-          "Basic TzhFUjFwSXdseFVmSTB1MHgxYXhyUEFtWEp4V3lOWWJxWXdpVmZ1Njoyc0EyNGxoSzBKRWdvNFlsRjd4c3pmYnZPV0M3UzZlSDJwcTVSWm8zZTdHTjdEY3JHSkZoNFcyckdsNXJ4N2ZCMlpPaFJHQ2tIU21WQjM0NlEyVVk3blZObzUzUVNMaWlWVXlzWTlOWE9ZdzZ4Uk43NEVMOE5SYW5haDJuUU42Tw==",
-        Accept: "application/json, text/plain, */*"
-      }
-    };
-    try {
-      console.log(JSON.parse(await rp(options)));
-      res.send(await rp(options));
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  getReviews();
+  Review.Review.find({}).then(results => {
+    res.send(results);
+  });
 });
 
 module.exports = router;
---
+++ feedback/server/udemyAPIHelper.js
@@ -0,0 +1,58 @@
+const rp = require("request-promise");
+const Review = require("./../database/index.js");
+
+async function getCourses() {
+  let data = "";
+  let courseReview = [];
+  let options = {
+    method: "GET",
+    uri: `https://www.udemy.com/api-2.0/courses`,
+    headers: {
+      Authorization:
+        "Basic TzhFUjFwSXdseFVmSTB1MHgxYXhyUEFtWEp4V3lOWWJxWXdpVmZ1Njoyc0EyNGxoSzBKRWdvNFlsRjd4c3pmYnZPV0M3UzZlSDJwcTVSWm8zZTdHTjdEY3JHSkZoNFcyckdsNXJ4N2ZCMlpPaFJHQ2tIU21WQjM0NlEyVVk3blZObzUzUVNMaWlWVXlzWTlOWE9ZdzZ4Uk43NEVMOE5SYW5haDJuUU42Tw==",
+      Accept: "application/json, text/plain, */*"
+    }
+  };
+  try {
+    data = JSON.parse(await rp(options));
+  } catch (err) {
+    console.log(err);
+  }
+  for (var i = 0; i < data.results.length; i++) {
+    async function getReviews(id) {
+      let options = {
+        method: "GET",
+        uri: `https://www.udemy.com/api-2.0/courses/${id}/reviews/?fields[course_review]=@all`,
+        headers: {
+          Authorization:
+            "Basic TzhFUjFwSXdseFVmSTB1MHgxYXhyUEFtWEp4V3lOWWJxWXdpVmZ1Njoyc0EyNGxoSzBKRWdvNFlsRjd4c3pmYnZPV0M3UzZlSDJwcTVSWm8zZTdHTjdEY3JHSkZoNFcyckdsNXJ4N2ZCMlpPaFJHQ2tIU21WQjM0NlEyVVk3blZObzUzUVNMaWlWVXlzWTlOWE9ZdzZ4Uk43NEVMOE5SYW5haDJuUU42Tw==",
+          Accept: "application/json, text/plain, */*"
+        }
+      };
+      let review;
+      try {
+        review = JSON.parse(await rp(options));
+      } catch (err) {
+        console.log(err);
+      }
+      var storage = {};
+      storage.id = id;
+      storage.review = review.results;
+      var reviewList = new Review.Review(storage);
+      reviewList
+        .save(function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("saved!");
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+    getReviews(data.results[i].id);
+  }
+}
+
+module.exports.getCourses = getCourses;
