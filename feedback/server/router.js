bf86c3f96d1e57f49cc9d82fa9c3e3bc269a7f85
Yuehan Duan <yuehanduan@yahoo.com>
2018-02-22 16:19:04 -0800

    Init async/await.

--- feedback/server/router.js
+++ feedback/server/router.js
@@ -3,23 +3,24 @@ var router = express.Router();
 var rp = require("request-promise");
 
 router.get("/", function(req, res) {
-  var options = {
-    method: "GET",
-    uri: "https://www.udemy.com/api-2.0/courses",
-    headers: {
-      Authorization:
-        "Basic TzhFUjFwSXdseFVmSTB1MHgxYXhyUEFtWEp4V3lOWWJxWXdpVmZ1Njoyc0EyNGxoSzBKRWdvNFlsRjd4c3pmYnZPV0M3UzZlSDJwcTVSWm8zZTdHTjdEY3JHSkZoNFcyckdsNXJ4N2ZCMlpPaFJHQ2tIU21WQjM0NlEyVVk3blZObzUzUVNMaWlWVXlzWTlOWE9ZdzZ4Uk43NEVMOE5SYW5haDJuUU42Tw==",
-      Accept: "application/json, text/plain, */*"
-    }
-  };
-  rp(options)
-    .then(function(course) {
-      console.log(JSON.parse(course));
-      res.send("Hello From Post");
-    })
-    .catch(function(err) {
+  async function getReviews() {
+    var options = {
+      method: "GET",
+      uri: "https://www.udemy.com/api-2.0/courses",
+      headers: {
+        Authorization:
+          "Basic TzhFUjFwSXdseFVmSTB1MHgxYXhyUEFtWEp4V3lOWWJxWXdpVmZ1Njoyc0EyNGxoSzBKRWdvNFlsRjd4c3pmYnZPV0M3UzZlSDJwcTVSWm8zZTdHTjdEY3JHSkZoNFcyckdsNXJ4N2ZCMlpPaFJHQ2tIU21WQjM0NlEyVVk3blZObzUzUVNMaWlWVXlzWTlOWE9ZdzZ4Uk43NEVMOE5SYW5haDJuUU42Tw==",
+        Accept: "application/json, text/plain, */*"
+      }
+    };
+    try {
+      console.log(JSON.parse(await rp(options)));
+      res.send(await rp(options));
+    } catch (err) {
       console.log(err);
-    });
+    }
+  }
+  getReviews();
 });
 
 module.exports = router;
