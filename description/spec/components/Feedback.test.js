e4057f5a7a0d1f3e4e6f711e3727edc10f62aa3e
James Davidson <jfdavidson10@gmail.com>
2018-02-27 13:25:19 -0800

    deleted old test

--- description/spec/components/Feedback.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import App from "../../client/components/App.jsx";
-
-test("should render Feedback correctly", () => {
-  const component = renderer.create(<App />);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
