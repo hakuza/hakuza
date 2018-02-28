542e7a1b4045c198283759640741dad79aa577d8
James Davidson <jfdavidson10@gmail.com>
2018-02-27 18:41:59 -0800

    working test base

--- description/client/components/App.jsx
+++ description/client/components/App.jsx
@@ -8,17 +8,20 @@ export default class App extends React.Component {
 
     this.state = {
       course: [],
-      currentCourse: ''
+      currentCourse: {}
     };
   }
 
   componentWillMount() {
-    axios.get('/description').then(response => {
-      this.setState({
-        course: response.data,
-        currentCourse: response.data[16]
-      });
-    });
+    axios
+      .get('/description')
+      .then(response => {
+        this.setState({
+          course: response.data,
+          currentCourse: response.data[16]
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -27,8 +30,11 @@ export default class App extends React.Component {
         <h1>{this.state.currentCourse.title}</h1>
         <h2>{this.state.currentCourse.headline}</h2>
         <Feedback currentCourse={this.state.currentCourse} />
-        <h4>{this.state.currentCourse.name}</h4>
-        <h4>{new Date(this.state.currentCourse.created).toDateString()}</h4>
+        <h4>Created by {this.state.currentCourse.name}</h4>
+        <h4>
+          Last Updated{' '}
+          {new Date(this.state.currentCourse.created).toDateString()}
+        </h4>
         <h4>English</h4>
         <h4>Closed captioning</h4>
       </div>
--- description/client/components/Feedback.jsx
+++ description/client/components/Feedback.jsx
@@ -4,9 +4,9 @@ const Feedback = props => {
   return (
     <div>
       <h4>histogram</h4>
-      <h4>{console.log(props)}</h4>
       <h4>{props.currentCourse.avg_rating}</h4>
-      <h4>{props.currentCourse.num_subscribers}</h4>
+      <h4>({props.currentCourse.num_reviews} ratings)</h4>
+      <h4>{props.currentCourse.num_subscribers} students enrolled</h4>
     </div>
   );
 };
