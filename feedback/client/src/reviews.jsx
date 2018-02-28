61465150753da9c19e924cf77709064f116c8584
Yuehan Duan <yuehanduan@yahoo.com>
2018-02-27 21:13:46 -0800

    Add dates and ratings to each review.

--- feedback/client/src/reviews.jsx
+++ feedback/client/src/reviews.jsx
@@ -19,9 +19,15 @@ export class Reviews extends React.Component {
                 <div className="indivContainer" key={i}>
                   <Picture name={elem.user.display_name} />
                   <div className="user" key={i + 1}>
+                    <div className="time" key={i + 2}>
+                      {elem.created}
+                    </div>
                     {elem.user.display_name}
                   </div>
-                  <div className="indivReviews" key={i + 2}>
+                  <div className="indivReviews" key={i + 3}>
+                    <div className="indivRating" key={i + 4}>
+                      Rating: {elem.rating}
+                    </div>
                     {elem.content}
                   </div>
                 </div>
