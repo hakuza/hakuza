e0971fc81dab600f253db5e2367e0ba216dc6b2d
Ben Blumenfeld <bmblumenfeld@gmail.com>
2018-02-28 14:31:58 -0800

    changed to match description

--- description/client/components/App.jsx
+++ description/client/components/App.jsx
@@ -1,43 +1,18 @@
-<<<<<<< HEAD
 import React from 'react';
 import axios from 'axios';
 import Feedback from './Feedback.jsx';
-=======
-import React from "react";
-import axios from "axios";
-import Feedback from "./Feedback.jsx";
->>>>>>> feature
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-<<<<<<< HEAD
       course: [],
       currentCourse: {}
     };
   }
 
   componentWillMount() {
-<<<<<<< HEAD
-    axios.get('/description').then(response => {
-      this.setState({
-        course: response.data,
-        currentCourse: response.data[16]
-=======
-      course: {}
-    };
-  }
-
-  componentDidMount() {
-    axios.get("/description").then(response => {
-      this.setState({
-        course: response.data
->>>>>>> feature
-      });
-    });
-=======
     axios
       .get('/description')
       .then(response => {
@@ -47,13 +22,11 @@ export default class App extends React.Component {
         });
       })
       .catch(err => console.log(err));
->>>>>>> master
   }
 
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <h1>{this.state.currentCourse.title}</h1>
         <h2>{this.state.currentCourse.headline}</h2>
         <Feedback currentCourse={this.state.currentCourse} />
@@ -63,14 +36,6 @@ export default class App extends React.Component {
           {new Date(this.state.currentCourse.created).toDateString()}
         </h4>
         <h4>English</h4>
-=======
-        <h1>Title Goes Here</h1>
-        <h2>Headline goes here</h2>
-        <Feedback />
-        <h4>Created by ....</h4>
-        <h4>Last Updated</h4>
-        <h4>Language</h4>
->>>>>>> feature
         <h4>Closed captioning</h4>
       </div>
     );
--- description/client/components/Feedback.jsx
+++ description/client/components/Feedback.jsx
@@ -1,28 +1,12 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React from "react";
->>>>>>> feature
 
 const Feedback = props => {
   return (
     <div>
       <h4>histogram</h4>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <h4>{console.log(props)}</h4>
-      <h4>{props.currentCourse.avg_rating}</h4>
-      <h4>{props.currentCourse.num_subscribers}</h4>
-=======
-      <h4>5 stars</h4>
-      <h4>ratings number</h4>
-      <h4>students enrolled</h4>
->>>>>>> feature
-=======
       <h4>{props.currentCourse.avg_rating}</h4>
       <h4>({props.currentCourse.num_reviews} ratings)</h4>
       <h4>{props.currentCourse.num_subscribers} students enrolled</h4>
->>>>>>> master
     </div>
   );
 };
