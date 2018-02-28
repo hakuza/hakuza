aef9e33e1a992fb1e73382f4a80fd3cf728a094e
James Davidson <jfdavidson10@gmail.com>
2018-02-28 11:38:30 -0800

    added enzyme adapter

--- description/client/tests/App.test.js
+++ description/client/tests/App.test.js
@@ -1,8 +1,13 @@
 import { shallow, mount } from 'enzyme';
 import React from 'react';
 import App from '../components/App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-it('works', () => {
+ReactDOM.render(<App />, document.getElementById('description'));
+
+it('works and doesnt crash', () => {
   const wrap = mount(<App />);
   expect(wrap).toMatchSnapshot();
 });
