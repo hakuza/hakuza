6ec1dff80a603177918ecc9a3857d913f12465cc
James Davidson <jfdavidson10@gmail.com>
2018-02-28 16:13:24 -0800

    dynamically rendered filled stars

--- description/client/components/App.jsx
+++ description/client/components/App.jsx
@@ -31,13 +31,17 @@ export default class App extends React.Component {
           <h1 id="title">{this.state.currentCourse.title}</h1>
           <h2>{this.state.currentCourse.headline}</h2>
           <Feedback currentCourse={this.state.currentCourse} />
-          <h4>Created by {this.state.currentCourse.name}</h4>
-          <h4>
-            Last Updated{' '}
-            {new Date(this.state.currentCourse.created).toDateString()}
-          </h4>
-          <h4>English</h4>
-          <h4>Closed captioning</h4>
+          <div id="by_line">
+            <h4 className="row_items">
+              Created by {this.state.currentCourse.name}
+            </h4>
+            <h4 className="row_items">
+              Last Updated{' '}
+              {new Date(this.state.currentCourse.created).toDateString()}
+            </h4>
+            <h4 className="row_items">English</h4>
+            <h4 className="row_items">Closed captioning</h4>
+          </div>
         </div>
       </div>
     );
--- description/client/components/Feedback.jsx
+++ description/client/components/Feedback.jsx
@@ -3,18 +3,24 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faStar from '@fortawesome/fontawesome-free-solid/faStar';
 
 const Feedback = props => {
+  let rating = props.currentCourse.avg_rating || 0;
+  let filledStars = [];
+  for (let i = 0; i < Math.floor(rating); i++) {
+    filledStars.push(
+      <FontAwesomeIcon key={i} icon={faStar} className="star" />
+    );
+  }
+  for (let i = 0; i < 5 - Math.floor(rating); i++) {
+    filledStars.push(<FontAwesomeIcon key={i + rating} icon={faStar} />);
+  }
   return (
-    <div>
-      <h4>
-        <FontAwesomeIcon icon={faStar} className="star" />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
+    <div id="container_feedback">
+      <h4 className="row_items">{filledStars.map(star => star)}</h4>
+      <h4 className="row_items">{props.currentCourse.avg_rating}</h4>
+      <h4 className="row_items">({props.currentCourse.num_reviews} ratings)</h4>
+      <h4 className="row_items">
+        {props.currentCourse.num_subscribers} students enrolled
       </h4>
-      <h4>{props.currentCourse.avg_rating}</h4>
-      <h4>({props.currentCourse.num_reviews} ratings)</h4>
-      <h4>{props.currentCourse.num_subscribers} students enrolled</h4>
     </div>
   );
 };
