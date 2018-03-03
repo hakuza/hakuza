8578d2b97a1eb2d0a87469024e14bdfaf36244e6
James Davidson <jfdavidson10@gmail.com>
2018-03-02 17:33:31 -0800

    added more css styling to what to learn box

--- expanded_description/client/index.js
+++ expanded_description/client/index.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import ReactHtmlParser, {processNodes, convertNodeToElement, htmlparser2} from 'react-html-parser';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import {faCheck} from '@fortawesome/fontawesome-free-solid';
+import faker from 'faker';
 import './styles.css';
 
 class App extends React.Component {
@@ -23,32 +24,50 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id="container">
         <div className="what_you_get">
-          <div>
-            <h3>What will I Learn?</h3>
-            <ul className="what_you_get_items">
-              <li>
-                <span><FontAwesomeIcon icon={faCheck}/></span>
-                How to lie to friends</li>
-              <li>
-                <span><FontAwesomeIcon icon={faCheck}/></span>
-                Who the spy REALLY</li>
-              <li>
-                <span><FontAwesomeIcon icon={faCheck}/></span>
-                How to save Merlin</li>
-              <li>
-                <span><FontAwesomeIcon icon={faCheck}/>
-                  Code names!</span>
-              </li>
-              <li>
-                <span><FontAwesomeIcon icon={faCheck}/>Trader Joes</span>
-              </li>
-              <li>
-                <span><FontAwesomeIcon icon={faCheck}/>
-                  Bitcoin?</span>
-              </li>
-            </ul>
+          <h3>What will I Learn?</h3>
+          <div id="columnHolder">
+            <div id="column1">
+              <ul className="what_you_get_items">
+                <li>
+                  <span><FontAwesomeIcon icon={faCheck}/></span>
+                  {faker
+                    .lorem
+                    .sentence()}</li>
+                <li>
+                  <span><FontAwesomeIcon icon={faCheck}/></span>
+                  {faker
+                    .lorem
+                    .sentence()}</li>
+                <li>
+                  <span><FontAwesomeIcon icon={faCheck}/></span>
+                  {faker
+                    .lorem
+                    .sentence()}</li>
+              </ul>
+            </div>
+            <div id="coloumn2">
+              <ul className="what_you_get_items">
+                <li>
+                  <span><FontAwesomeIcon icon={faCheck}/>
+                  </span>{faker
+                    .lorem
+                    .sentence()}
+                </li>
+                <li>
+                  <span><FontAwesomeIcon icon={faCheck}/></span>{faker
+                    .lorem
+                    .sentence()}
+                </li>
+                <li>
+                  <span><FontAwesomeIcon icon={faCheck}/>
+                  </span>{faker
+                    .lorem
+                    .sentence()}
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <h3>Requirements</h3>
