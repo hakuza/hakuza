6ee6d04533cb9cbdb6bd1f0ef7742a12bbb066f9
James Davidson <jfdavidson10@gmail.com>
2018-03-02 16:13:40 -0800

    added what you learn box

--- expanded_description/client/index.js
+++ expanded_description/client/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ReactHtmlParser, {processNodes, convertNodeToElement, htmlparser2} from 'react-html-parser';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import {faCheck} from '@fortawesome/fontawesome-free-solid';
+import './styles.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -21,6 +24,33 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <div className="what_you_get">
+          <div>
+            <h3>What will I Learn?</h3>
+            <ul className="what_you_get_items">
+              <li>
+                <span><FontAwesomeIcon icon={faCheck}/></span>
+                How to lie to friends</li>
+              <li>
+                <span><FontAwesomeIcon icon={faCheck}/></span>
+                Who the spy REALLY</li>
+              <li>
+                <span><FontAwesomeIcon icon={faCheck}/></span>
+                How to save Merlin</li>
+              <li>
+                <span><FontAwesomeIcon icon={faCheck}/>
+                  Code names!</span>
+              </li>
+              <li>
+                <span><FontAwesomeIcon icon={faCheck}/>Trader Joes</span>
+              </li>
+              <li>
+                <span><FontAwesomeIcon icon={faCheck}/>
+                  Bitcoin?</span>
+              </li>
+            </ul>
+          </div>
+        </div>
         <h3>Requirements</h3>
         <ul>
           <li>Basic computer skills</li>
