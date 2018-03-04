1e90e243ed7e345a3c1d9d76bbe351a8c1ee17a8
Jason <antherius47@gmaio.com>
2018-03-04 15:27:47 -0800

    Added "show more" button   -simple functionality, need to add styling

--- compare_courses/client/components/courseList.jsx
+++ compare_courses/client/components/courseList.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import Course from './course.jsx';
+import {Collapse} from 'react-collapse';
 
-function CourseList({ courses, onclick }) {
-  return (
-    <div className="course-list">
-      {courses.map(course => (
-        <Course key={course.id} course={course} onclick={onclick} />
-      ))}
-    </div>
-  );
+export default class CourseList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 515,
+      expanded: false,
+    }
+    this.accordian = this.accordian.bind(this);
+  }
+
+  accordian() {
+    let expanded = !this.state.expanded;
+    this.setState({
+      height: expanded ? 1115 : 515,
+      expanded: expanded,
+    });
+    // console.log(this.state);
+  }
+  
+  render() {
+    return (
+      <div>
+        <button onClick={this.accordian}>Show More</button>
+        <Collapse isOpened={true} fixedHeight={this.state.height}>
+          <div className="course-list">
+            {this.props.courses.map(course => (
+              <Course key={course.id} course={course} onclick={this.props.onclick} />
+            ))}
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
 }
 
-export default CourseList;
+// export default CourseList;
