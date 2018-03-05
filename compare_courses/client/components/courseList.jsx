3942af790e6050442c6ec80a58b27f21a63acf0e
Jason <antherius47@gmaio.com>
2018-03-04 16:07:17 -0800

    Modified style of View More button

--- compare_courses/client/components/courseList.jsx
+++ compare_courses/client/components/courseList.jsx
@@ -9,28 +9,41 @@ export default class CourseList extends React.Component {
     this.state = {
       height: 515,
       expanded: false,
+      'view-more-styles': {
+        top: 515,
+      }
     }
     this.accordian = this.accordian.bind(this);
   }
 
   accordian() {
     let expanded = !this.state.expanded;
+    // let height = expanded ? 1115 : 515;
     this.setState({
       height: expanded ? 1115 : 515,
       expanded: expanded,
+      'view-more-styles': {
+        top: expanded ? 1135 : 515,
+      },
     });
-    // console.log(this.state);
   }
   
   render() {
     return (
       <div>
-        <button onClick={this.accordian}>Show More</button>
         <Collapse isOpened={true} fixedHeight={this.state.height}>
           <div className="course-list">
             {this.props.courses.map(course => (
               <Course key={course.id} course={course} onclick={this.props.onclick} />
             ))}
+          </div>
+          <div className="view-more-container" style={this.state['view-more-styles']}>
+            <button 
+              className="view-more-btn"
+              onClick={this.accordian}
+            >
+              {this.state.expanded ? '- View Less' : '+ View More'}
+            </button>
           </div>
         </Collapse>
       </div>
